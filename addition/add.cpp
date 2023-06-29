3daf818e195518ff9749e759367bca8357adae21
Anush-Krish <107431883+Anush-Krish@users.noreply.github.com>
2023-06-29 18:41:38 +0530

    first push

---
+++ addition/add.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Anush Krishna on 29/06/23.
+//
+
+#include "add.h"
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int add =2+2;
+    std::cout << "Addition "<< add;
+    return 0;
+}
+
---
+++ addition/add.h
@@ -0,0 +1,14 @@
+//
+// Created by Anush Krishna on 29/06/23.
+//
+
+#ifndef PRACTICE_ADD_H
+#define PRACTICE_ADD_H
+
+
+class add {
+
+};
+
+
+#endif //PRACTICE_ADD_H
